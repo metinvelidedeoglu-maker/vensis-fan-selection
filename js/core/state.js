9437f96e57a677ce19efb45e2d34bec5b3267011
metinvelidedeoglu-maker <metinvelidedeoglu@gmail.com>
2026-07-19 04:02:51 +0300

    Densify all fan performance curves at startup

--- js/core/state.js
+++ js/core/state.js
@@ -1,6 +1,16 @@
 (function(){
+  const U=window.VensisUtils;
+  const models=(window.models||[]).map(model=>{
+    const sourcePoints=(model.points||[]).map(point=>[Number(point[0]),Number(point[1])]);
+    return {
+      ...model,
+      sourcePoints,
+      points:U?.densifyPoints?U.densifyPoints(sourcePoints,201):sourcePoints
+    };
+  });
+  window.models=models;
   window.VensisState={
-    models:window.models||[],
+    models,
     results:[],
     selectedTags:new Set(),
     selectedSeries:new Set(),
