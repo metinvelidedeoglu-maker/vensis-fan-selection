41467da77d545e814226b250a17e5f16b1a3bcf3
metinvelidedeoglu-maker <metinvelidedeoglu@gmail.com>
2026-08-31 20:00:18 +0300

    Remove unused Vortice route guard file

--- js/vortice-route-guard.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  'use strict';
-  const path=(location.pathname||'').toLowerCase();
-  if(!path.endsWith('/catalog-vortice.html'))return;
-  const params=new URLSearchParams(location.search);
-  if(params.get('series'))return;
-  location.replace('catalog-vortice-stable.html'+(location.search||''));
-})();
