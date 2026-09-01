9dd5546b68b19bcd244549ac266d64a1287cb10c
metinvelidedeoglu-maker <metinvelidedeoglu@gmail.com>
2026-09-01 12:37:01 +0300

    Load quotation output cleanup

--- js/quotation-formats.js
+++ js/quotation-formats.js
@@ -3,12 +3,21 @@
   if(typeof module==='object'&&module.exports)module.exports=api;
   else{
     root.VensisQuotationFormats=api;
-    if(typeof document!=='undefined'&&!document.getElementById('vensisDocumentOutputI18n')){
-      const script=document.createElement('script');
-      script.id='vensisDocumentOutputI18n';
-      script.src='js/document-output-i18n.js?v=20260901-r1';
-      script.defer=true;
-      document.head.appendChild(script);
+    if(typeof document!=='undefined'){
+      if(!document.getElementById('vensisDocumentOutputI18n')){
+        const script=document.createElement('script');
+        script.id='vensisDocumentOutputI18n';
+        script.src='js/document-output-i18n.js?v=20260901-r2';
+        script.defer=true;
+        document.head.appendChild(script);
+      }
+      if(!document.getElementById('vensisQuotationOutputCleanup')){
+        const script=document.createElement('script');
+        script.id='vensisQuotationOutputCleanup';
+        script.src='js/quotation-output-cleanup.js?v=20260901-r1';
+        script.defer=true;
+        document.head.appendChild(script);
+      }
     }
   }
 })(typeof globalThis!=='undefined'?globalThis:this,function(){
