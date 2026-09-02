feaf112138a4893ce5c0b0ad01e3ed5559e2937f
metinvelidedeoglu-maker <metinvelidedeoglu@gmail.com>
2026-09-02 20:31:22 +0300

    Load Vitlo quote prices before catalog registry

--- data/series-overrides.js
+++ data/series-overrides.js
@@ -26,7 +26,8 @@ if(!window.VensisCatalogBrand||window.VensisCatalogBrand==='vitlo'){
   document.write(
     '<script src="data/fans-16.js?v=20260902-crs-crh-31-r1"><\/script>',
     '<script src="data/fans-17.js?v=20260902-crs-crh-63-r1"><\/script>',
-    '<script src="data/cr-family-matrix.js?v=20260902-cr-matrix-r1"><\/script>'
+    '<script src="data/cr-family-matrix.js?v=20260902-cr-matrix-r1"><\/script>',
+    '<script src="data/vitlo-quote-prices-20260902.js?v=20260902-r1"><\/script>'
   );
 }
 
