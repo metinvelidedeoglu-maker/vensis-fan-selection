e857e65fcf296034bbc3f1450f80e21532214ad8
metinvelidedeoglu-maker <metinvelidedeoglu@gmail.com>
2026-09-08 13:29:59 +0300

    Load quotation SILENT correction layer

--- data/series-overrides.js
+++ data/series-overrides.js
@@ -47,6 +47,10 @@ if(!window.VensisCatalogBrand||window.VensisCatalogBrand==='sp'){
   );
 }
 
+if(/(^|\/)quotation\.html$/.test(String(location.pathname||''))){
+  document.write('<script src="js/quotation-silent-fix.js?v=20260908-r1"><\/script>');
+}
+
 document.write('<script src="js/catalog-model-seo-links.js?v=20260902-r1"><\/script>');
 
 (function removeCatalogImageErrorHandlers(){
